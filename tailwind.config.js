7e1e9fc369c6a9004856e69236fe3ea8b8226878
Joe Lyles <lylesjoe@protonmail.com>
2024-07-20 21:44:45 -0400

    portfolio setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'tahoma': ['Tahoma', 'serif'],
+      'firacode': ['Fira Code', 'monospace'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
